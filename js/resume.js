e10fbbbca06d5351c821fcd44a272f4964145c1e
whale <cloisonne320@sina.com>
2016-09-02 17:12:10 +0800

    更新

---
+++ js/resume.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+    	navigation:'true',
+    	navigationColor:'#fff',
+    	sectionsColor:['#4c8dae','#eedeb0','#f3f9f1'],
+    	paddingTop:'10px'
+
+    });
+});
