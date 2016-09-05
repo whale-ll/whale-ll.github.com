90049ee9a481335aa3068320752ce6b8018c180c
whale <cloisonne320@sina.com>
2016-09-05 17:47:45 +0800

    delete

--- js/resume.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage({
-    	navigation:'true',
-    	navigationColor:'#fff',
-    	sectionsColor:['#4c8dae','#eedeb0','#f3f9f1'],
-    	paddingTop:'10px'
-
-    });
-});
